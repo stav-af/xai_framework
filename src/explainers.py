ea764cafc61ba2b3206eccde2997227924de77aa
stav-ej <stavarmonifriedmann@gmail.com>
2024-10-15 11:45:02 +0100

    Clustering to fit

--- src/explainers.py
+++ src/explainers.py
@@ -3,7 +3,7 @@ import torch
 
 def shap_explainer(model, data):
     explainer = KernelShap(model.forward)
-    explanation = explainer.attribute(torch.tensor(data, dtype=torch.float32, requires_grad=True), perturbations_per_eval=500).tolist()
+    explanation = explainer.attribute(torch.tensor(data, dtype=torch.float32, requires_grad=True))
 
     return explanation
 
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import torch
 import dataloader
+from sklearn.cluster import KMeans
 
 from models import cross_val_train, FeedForwardNN
 from explainers import shap_explainer
@@ -23,34 +24,41 @@ def train_models():
         torch.save(model.state_dict(), f'models/{file.replace("csv", "pth")}')
 
 
-
-
-
-
 def main():
-    model_path = "XOR.pth"
+    model_path = "OR.pth"
     print(model_path)
 
     X, y, exp = dataloader.load(f'data/{model_path.replace("pth", "csv")}')
 
     model = FeedForwardNN(X.shape[1])
     model.load_state_dict(torch.load(f"models/{model_path}"))
+    alpha = 0.1
+    for i in range(1, X.size + 1):
+        explanation = shap_explainer(model, X[i - 1: i])[0]
+        # cutoff = torch.median(explanation)
 
-    sample_size = 10
-    avg = [0] * 12
-    for explanation in shap_explainer(model, X[:sample_size]):
-        for i, val in enumerate(explanation):
-            avg[i] += val
+        # extracted_explanation = (explanation > cutoff).nonzero(as_tuple=True)[0]
 
-    for i, val in enumerate(avg):
-        avg[i] = val/sample_size
+        # print(explanation)
+        # print(extracted_explanation)
+        abs_explanation = torch.abs(explanation)
+
+        # Initialize EMA as the first element
+        ema = abs_explanation[0]
+
+        # Calculate EMA across the explanation values
+        for value in abs_explanation[1:]:
+            ema = alpha * value + (1 - alpha) * ema  # EMA update formula
+
+        # Get the indices of the values that are larger than the EMA
+        large_value_indices = (abs_explanation > ema).nonzero(as_tuple=True)[0]
+        print(large_value_indices)
+        print(explanation)
+        print(y[i])
 
     print(avg)
     avg = [0 if val < 0.1 else 1 for val in avg]
     print(avg)
-        
-
-
 
 if __name__ == "__main__":
     main()
